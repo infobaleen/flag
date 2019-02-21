e545d8237c6a29b38ed807cac7abb35f74cedca6
Florian Uekermann <florian@uekermann.me>
2019-02-21 16:35:03 +0100

    add uint8

--- uint.go
+++ uint.go
@@ -51,3 +51,28 @@ func (f FlagSet) U32R(flag string, description string) FlagU32 {
 func (f FlagSet) U32D(flag string, defaultValue uint32, description string) FlagU32 {
 	return f.u32(flag, defaultValue, description, false)
 }
+
+type FlagU8 struct{ value *uint8 }
+
+func (f FlagU8) Value() uint8 { return *f.value }
+
+func (f FlagU8) SetValue(value string) (err error) {
+	tmp, err := strconv.ParseUint(value, 10, 8)
+	*f.value = uint8(tmp)
+	return
+}
+
+
+func (f FlagSet) u8(flag string, defaultValue uint8, description string, required bool) FlagU8 {
+	var handle = FlagU8{value: &defaultValue}
+	f[flag] = FlagState{Handle: handle, Type: "uint8", Description: description, Required: required, Default: fmt.Sprint(defaultValue)}
+	return handle
+}
+
+func (f FlagSet) U8R(flag string, description string) FlagU8 {
+	return f.u8(flag, 0, description, true)
+}
+
+func (f FlagSet) U8D(flag string, defaultValue uint8, description string) FlagU8 {
+	return f.u8(flag, defaultValue, description, false)
+}
